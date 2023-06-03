55d9cb8b75b321ac06f92e724baf82150678d38a
Fadhil <fadhilsaheer@gmail.com>
2023-06-03 22:19:38 +0530

    feat: file api

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
+import fs from 'fs';
 import http from 'http';
 import cors from 'cors';
 import express from 'express';
+import multer from 'multer';
 import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { schema } from './graphql';
@@ -12,6 +14,46 @@ const graphqlServer = new ApolloServer({ schema });
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
+
+// configure file api
+let storage = multer.diskStorage({
+  destination: (_, __, cb) => cb(null, 'uploads/'),
+  filename: (_, file, cb) => {
+    let extArray = file.mimetype.split('/');
+    let extension = extArray[extArray.length - 1];
+    cb(null, Date.now() + '.' + extension);
+  },
+});
+
+const upload = multer({ storage });
+
+app.use('/file', express.static('uploads')); // serving uploaded files to `file/${filename}`
+
+app.post('/file', upload.single('uploaded_file'), (req, res) => {
+  let path = `${req.file?.filename}`;
+  res.json(path);
+});
+
+app.delete('/file/:filename', (req, res) => {
+  let filename = req.params?.filename;
+
+  if (!filename) {
+    res.status(422).json('Filename is not provided');
+    return;
+  }
+
+  let filepath = `./uploads/${filename}`;
+
+  fs.unlink(filepath, (err) => {
+    if (err && err.code == 'ENOENT') {
+      res.status(404).json('File does not exist.');
+    } else if (err) {
+      res.status(500).json('Failed to delete file.');
+    } else {
+      res.status(200).json('Deleted successfully.');
+    }
+  });
+});
 
 const startServer = async () => {
   await graphqlServer.start();
@@ -32,5 +74,6 @@ const startServer = async () => {
 startServer()
   .then(() => {
     console.log(`[🚀] GRAPHQL SERVER AT http://localhost:${PORT}/graphql`);
+    console.log(`[🚀] FILE API AT http://localhost:${PORT}/file`);
   })
   .catch(console.log);
