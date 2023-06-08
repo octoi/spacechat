5574679018a2878fa59bc989a168df9731b6daa5
Fadhil <fadhilsaheer@gmail.com>
2023-06-08 22:23:13 +0530

    new chat wrapper

---
+++ app/components/sidebar/newChatWrapper.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { ReactComponent } from '@/lib/types/react.type';
+
+export const NewChatWrapper: ReactComponent = ({ children }) => {
+  return <div>NewChatWrapper</div>;
+};
