9a49a7ab55d3963065c733979b41010c2e7cd960
Fadhil <fadhilsaheer@gmail.com>
2023-08-14 15:13:15 +0530

    remove: graphql folder

--- server/src/graphql/index.ts
+++
@@ -1,8 +0,0 @@
-import { GraphQLSchema } from 'graphql';
-import { Queries } from './queries';
-import { Mutations } from './mutations';
-
-export const schema = new GraphQLSchema({
-  query: Queries,
-  mutation: Mutations,
-});
