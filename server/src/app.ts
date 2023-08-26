4f3208523114d3e7341984846d52bd07ef485ac2
Fadhil <fadhilsaheer@gmail.com>
2023-08-26 14:23:09 +0530

    feat: init socket.io

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,16 @@
+import http from 'http';
 import cors from 'cors';
 import express from 'express';
+import { Server as SocketServer } from 'socket.io';
 import { fileRouter } from './routers/file.router';
 import { userRouter } from './routers/user.router';
 import { messageRouter } from './routers/message.router';
 
 const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server);
 
+// express middleware
 app.use(cors());
 app.use(express.json());
 
@@ -15,6 +20,10 @@ app.use('/file', fileRouter);
 app.use('/user', userRouter);
 app.use('/message', messageRouter);
 
+// connect socket.io
+io.listen(server, { cors: { origin: '*' } });
+io.on('connection', (socket) => {});
+
 // listen to port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`[🚀] http://localhost:${PORT}`));
+server.listen(PORT, () => console.log(`[🚀] http://localhost:${PORT}`));
