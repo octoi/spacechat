6ced7b06615256cbf00ed2485772479a424c8986
Fadhil <fadhilsaheer@gmail.com>
2023-08-29 09:09:28 +0530

    socket.io events

--- server/src/socketIo/types.ts
+++ server/src/socketIo/types.ts
@@ -7,6 +7,32 @@ export enum Events {
   Disconnect = 'disconnect',
 }
 
-export interface ServerToClientEvents {}
+type CallbackFn = (data: {
+  status: boolean;
+  message?: string;
+  data?: any;
+}) => void;
 
-export interface ClientToServerEvents {}
+export interface ServerToClientEvents {
+  message: (message: {
+    id: number;
+    type: 'TEXT' | 'VOICE' | 'IMAGE' | 'VIDEO' | 'DOCUMENT';
+    message: string;
+    createdAt: any;
+    status: 'SENT' | 'RECEIVED' | 'SEEN';
+  }) => void;
+}
+
+export interface ClientToServerEvents {
+  connect: (data: { userId: number }) => void; // store userID and socketId to redis
+  message: (
+    message: {
+      senderId: number;
+      targetId: number;
+      type: 'TEXT' | 'VOICE' | 'IMAGE' | 'VIDEO' | 'DOCUMENT';
+      message: string;
+    },
+    callback: CallbackFn
+  ) => void; //  store & send message to target
+  markAsSeen: (data: { targetId: number }, callback: CallbackFn) => void; // mark target messages as seen
+}
