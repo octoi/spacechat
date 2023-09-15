b309c43d6f3b5e658c20868081a20a1ec306cd41
Fadhil <fadhilsaheer@gmail.com>
2023-09-15 05:31:04 +0530

    user type

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -4,3 +4,12 @@ import { PropsWithChildren } from 'react';
 export type ReactComponent<Props = {}> = React.FC<PropsWithChildren<Props>>;
 
 export type SetState<T> = React.Dispatch<React.SetStateAction<T>>;
+
+export interface UserType {
+  id: number;
+  username: string;
+  name: string;
+  profile: string;
+  about: string;
+  token: string;
+}
