3a09d739fee58ffb675e5b60cb007255dc53fbf4
Fadhil <fadhilsaheer@gmail.com>
2023-09-16 10:28:46 +0530

    fix: authwrap

--- app/components/auth-wrap.tsx
+++ app/components/auth-wrap.tsx
@@ -3,16 +3,17 @@ import { ReactComponent } from '@/lib/types';
 import { useRouter } from 'next/router';
 import { userStore } from '@/store/user.store';
 import { Paths } from '@/lib/constants';
+import { useStore } from 'zustand';
 
 export const AuthWrap: ReactComponent = ({ children }) => {
   const router = useRouter();
-  const { user } = userStore.getState();
+  const { user } = useStore(userStore);
 
   useEffect(() => {
     if (!user) {
       router.push(Paths.login); // passing current path in order to redirect back to same page after login
     }
-  }, []);
+  }, [user]);
 
   return children;
 };
