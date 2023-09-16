70a700416028cb17b01d21db98219705a0e5ebb7
Fadhil <fadhilsaheer@gmail.com>
2023-09-16 10:29:53 +0530

    sidebar layout

--- app/components/sidebar/index.tsx
+++ app/components/sidebar/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { SidebarHeader } from './header';
+import { ChatList } from './chat-list';
 
 export const Sidebar: React.FC = () => {
-  return <div>Sidebar</div>;
+  return (
+    <div className='bg-app-dark1 w-full max-w-lg h-screen overflow-y-scroll'>
+      <SidebarHeader />
+      <ChatList />
+    </div>
+  );
 };
