e95e247dc64da9d7c652ff1c5d1fddc3b42a2571
Fadhil <fadhilsaheer@gmail.com>
2023-09-21 14:59:23 +0530

    feat: chat list store

---
+++ app/store/chatlist.store.ts
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { showToast } from '@/lib/show-toast';
+import { ChatListType } from '@/lib/types';
+import { getFullURL } from '@/lib/url';
+import { createStore } from 'zustand/vanilla';
+import { userStore } from './user.store';
+
+interface ChatListStore {
+  chats: ChatListType[];
+  chatsLoading: boolean;
+  loadChats: () => Promise<void>;
+}
+
+export const chatListStore = createStore<ChatListStore>((set) => ({
+  chats: [],
+  chatsLoading: false,
+  loadChats() {
+    return new Promise((resolve, reject) => {
+      set({ chatsLoading: true });
+      axios
+        .get(getFullURL('/user/chat'), {
+          headers: {
+            Authorization: `Bearer ${userStore.getState().user?.token}`,
+          },
+        })
+        .then(({ data }) => {
+          set({ chats: data });
+          resolve();
+        })
+        .catch((err) => {
+          let details = {
+            title: 'Failed to send request',
+            description: err?.message,
+          };
+
+          if (err?.response?.data) {
+            details.title = 'Failed to retrieve chats';
+            details.description = err.response.data.message;
+          }
+
+          showToast({
+            ...details,
+            status: 'error',
+          });
+
+          set({ chats: [] });
+          reject();
+        })
+        .finally(() => set({ chatsLoading: false }));
+    });
+  },
+}));
